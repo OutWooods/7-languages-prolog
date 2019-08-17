3704dc8dedab7954bc6ad3d7e3dace56afa41cd7
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-08-17 10:39:13 +0100

    add notes on writing in prolog

--- day-3-questions.pl
+++ day-3-questions.pl
@@ -1,8 +1,17 @@
 %-- Prolog has some input/output features - find print predicates that print out variables
-%-- Find a way to use print predicates to print only successful solutions. How do they work? 
+%-- So the more flexible seems to be format - here you can give if a template and it'll try and match the template to that output
 
-%-- Make a soduku solver to work on 9 X 9 puzzles
+Write(Argument).
+
+format('%d %d %d | %d %d %d', [1,2,3,1,2,3]),
+
+%-- Find a way to use print predicates to print only successful solutions. How do they work? 
+%-- if you only want it to print if its true. You just put the print after the checks. That way if its fails, they will not run.
+test(X) :- 
+  X is 1,
+  format('%d %d %d | %d %d %d', [1,2,3,1,2,3]),
+  write("Hello").
+
+%-- Make a Soduku solver to work on 9 X 9 puzzles
 
 %-- Make a Sudoku solver print prittier solutions
-
-%-- Make a Eight queens answer that takes a list of queens. Represent each queen as an integer from 1-8. Get the row of a queen by its position in the list and the column by the value in the list.
