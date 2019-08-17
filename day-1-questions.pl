ba79f873163d6a497b8d6e9c53922d6cc690127e
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-08-17 10:40:53 +0100

    rename file to day 1 questions

---
+++ day-1-questions.pl
@@ -0,0 +1,15 @@
+%-- To find who plays guitar you just do ```play(Musician, guitar).```
+%-- To find who plays guitar and country music you just do ```play(Musician, guitar), genre(Musician, country).```
+
+play(smith, guitar).
+play(smith, sing).
+play(kesha, sing).
+play(taylor_swift, sing).
+play(taylor_swift, guitar).
+
+genre(hendrix, rock).
+genre(smith, rock).
+genre(kesha, pop).
+genre(taylor_swift, pop).
+genre(taylor_swift, country).
+
