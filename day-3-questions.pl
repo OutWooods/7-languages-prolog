2f8f0543dc8c5a7465ace0049619aac2219c4600
Tom Grand <24781073+Tagrand@users.noreply.github.com>
2019-08-22 16:27:59 +0100

    reflect on solutions

--- day-3-questions.pl
+++ day-3-questions.pl
@@ -14,42 +14,44 @@ test(X) :-
 
 %-- Make a Soduku solver to work on 9 X 9 puzzles
 
-%-- Rule one - board should be a solution
 %-- Im going for a lists as row so you provide 9 lists with 9 rows.
 
+%-- Rule one - only 1- 9 on the board
 valid_numbers([],[]).
 valid_numbers([Row|Remaining],[Row|Validated]) :-
     fd_domain(Row, 1, 9),
     valid_numbers(Remaining, Validated).
 
-valid_row([],[]).
-valid_row([Row|Remaining],[Row|Validated]) :- 
+%-- Rule two - all numbers should be unique in each row
+valid_rows([],[]).
+valid_rows([Row|Remaining],[Row|Validated]) :- 
     fd_all_different(Row),
     valid_row(Remaining, Validated).
 
+%-- Rule three - all numbers should be unique in each column
 first_value([], [], []).
-first_value([[Start|Rest]|Remaining], [Start|Beginning], [Rest|End]) :- 
-    first_value(Remaining, Beginning, End).
+first_value([[First|Rest]|Remaining], [First|FirstValues], [Rest|Tails]) :- 
+    first_value(Remaining, FirstValues, Tails).
 
-valid_column([[],[],[],[],[],[],[],[],[]], []).
-valid_column(Board, [Column|Columns]) :- 
+valid_columns([[],[],[],[],[],[],[],[],[]], []).
+valid_columns(Board, [Column|Columns]) :- 
     first_value(Board, Column, Remaining),
     fd_all_different(Column),
     valid_column(Remaining, Columns).
 
-first_3([]).
-first_3([First, Second, Third|Rest]) :- 
+%-- Rule four - all numbers should be unique in each 3x3 box
+valid_boxes([]).
+valid_boxes([First, Second, Third|Rest]) :- 
     First = [],
     first_3(Rest, Remaining).
-first_3([First,Second,Third|Rest]) :-
+valid_boxes([First,Second,Third|Rest]) :-
     append([A,B,C], Rest1, First),
     append([D,E,F], Rest2, Second),
     append([G,H,I], Rest3, Third),
     fd_all_different([A,B,C,D,E,F,G,H,I,J]),
     first_3([Rest1, Rest2, Rest3|Rest]).
     
-%-- get the same index of every row and put them into a new array. ALl values at the same index should be true.
-    
+%-- All put together in one sudoku board
 sudoku(Board, Solution) :- 
     Board = Solution,
     Board = [[_,_,_,_,_,_,_,_,_],
@@ -64,6 +66,8 @@ sudoku(Board, Solution) :-
     valid_numbers(Board, Solution),
     valid_row(Board, Solution),
     valid_column(Board, Transposed),
-    first_3(Board).
+    valid_boxes(Board).
 
-%-- Make a Sudoku solver print prittier solutions
+%-- In the future I would like to look at making a nicer printer, also making a solution that works for any sized board (the valid_columns is hard coded for a 9 X 9 grid.
+%-- Also alot of the rules have alot of responsibilities, I would like to break them down into simpler forms.
+
